17aa1578b6d0b832ca31c1cca4e456baf2cc2890
rodchen <rodchen_king@outlook.com>
2022-01-06 15:30:03 +0800

    1. 添加eslit & prettier & pre-commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*
+ * @Description:
+ * @Author: rodchen
+ * @Date: 2022-01-06 11:17:01
+ * @LastEditTime: 2022-01-06 14:39:45
+ * @LastEditors: rodchen
+ */
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,8 +1,15 @@
-import { ReportHandler } from 'web-vitals';
+/*
+ * @Description: 
+ * @Author: rodchen
+ * @Date: 2022-01-06 11:17:01
+ * @LastEditTime: 2022-01-06 14:40:05
+ * @LastEditors: rodchen
+ */
+import { ReportHandler } from "web-vitals";
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
